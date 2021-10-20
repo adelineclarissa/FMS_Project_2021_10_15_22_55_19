b95cba649886643d72e17a48e4a6c99ebe316a8e
adelineclarissa <adeline.clarissa@gmail.com>
2021-10-20 15:26:46 -0700

    This is the starting code

--- sketch.js
+++ sketch.js
@@ -1,30 +1,45 @@
 let canvasWidth = 600, canvasHeight = 400;
+let balsamiq;
+let start_button, ins_button;
+
+function preload() {
+  balsamiq = loadfont('assets/BalsamiqSans-Bold.ttf');
+}
 
 function setup() {
-  createCanvas(canvasWidth, canvasHeight);
+  createCanvas(windowWidth, windowHeight);
   background('#9BDEEB');
+
+  start_button = createButton("START");
+  start_button.position(windowWidth*3/5,windowHeight*1/3);
+  start_button.onMouseReleased(window.location = "menu.html");
+
+  ins_button = createButton("INSTRUCTIONS");
+  start_button.position(windowWidth*3/5,windowHeight*2/3);
+  ins_button.onMouseReleased(instructions());
+
 }
 
 function draw() {
-  
-  startButton();
-  insButton();
 
 }
 
-function startButton() {
+function createStartButton() {
   noStroke();
   fill('#FAD518');
   rect(280, 150, 220, 55, 30);
   textSize(32);
-  fill('#636161');
-  text('START', 10, 30);
+  fill('#000000');
+  text('START', 340, 190);
 }
 
-function insButton() {
+function createInsButton() {
   noStroke();
   fill('#E56365');
   rect(280, 220, 220, 55, 30);
+  textSize(24);
+  fill('#000000');
+  text('INSTRUCTIONS', 300, 257);
 }
 
 function start() {
