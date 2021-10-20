d53b589b4874ae2afc56856d0bf7da634d52b8c6
adelineclarissa <adeline.clarissa@gmail.com>
2021-10-20 15:55:31 -0700

    This is the newest release

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let balsamiq;
 let start_button, ins_button;
 
 function preload() {
-  balsamiq = loadfont('assets/BalsamiqSans-Bold.ttf');
+  balsamiq = loadFont('assets/BalsamiqSans-Bold.ttf');
 }
 
 function setup() {
@@ -12,40 +12,22 @@ function setup() {
 
   start_button = createButton("START");
   start_button.position(windowWidth*3/5,windowHeight*1/3);
-  start_button.onMouseReleased(window.location = "menu.html");
+  start_button.mouseReleased(toMenu);
 
   ins_button = createButton("INSTRUCTIONS");
-  start_button.position(windowWidth*3/5,windowHeight*2/3);
-  ins_button.onMouseReleased(instructions());
+  ins_button.position(windowWidth*3/5,windowHeight*2/3);
+  ins_button.mouseReleased(toInstructions);
 
+}
+
+function toMenu(){
+  window.location = "menu.html"
+}
+
+function toInstructions() {
+  window.location = "instructions.html"
 }
 
 function draw() {
 
-}
-
-function createStartButton() {
-  noStroke();
-  fill('#FAD518');
-  rect(280, 150, 220, 55, 30);
-  textSize(32);
-  fill('#000000');
-  text('START', 340, 190);
-}
-
-function createInsButton() {
-  noStroke();
-  fill('#E56365');
-  rect(280, 220, 220, 55, 30);
-  textSize(24);
-  fill('#000000');
-  text('INSTRUCTIONS', 300, 257);
-}
-
-function start() {
-  print("start button is pressed");
-}
-
-function instructions() {
-  print("instructions button is pressed");
 }
