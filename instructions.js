0cea6901c1069da30d8c46160501bd26b3571639
adelineclarissa <adeline.clarissa@gmail.com>
2021-10-20 16:08:21 -0700

    the home button will direct the user to the homepage instead of the menu

--- instructions.js
+++ instructions.js
@@ -1,9 +1,13 @@
 function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background('#9BDEEB');
+  
     homeButton = createButton("Home");
     homeButton.position(0,0);
     homeButton.mouseReleased(toHome);
-}
-
-function toHome() {
-    window.location = "menu.html";
-}
+  
+  }
+  
+  function toHome() {
+    window.location = "home.html";
+  }
