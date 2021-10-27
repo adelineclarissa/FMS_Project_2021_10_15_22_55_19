0063d99f703afda9439bf7be7da166b2385456fd
daniel Burgin <daburgin@asu.edu>
2021-10-27 16:22:45 -0700

    Co-authored-by: Gourav-praneeth <Gourav-praneeth@users.noreply.github.com>

--- tracinggame.js
+++ tracinggame.js
@@ -6,8 +6,20 @@ function setup() {
   homeButton.position(0,0);
   homeButton.mouseReleased(toHome);
 
+  resetButton = createButton("Home");
+  homebutton.position(windowWidth, windowHeight)
+  homebutton.mousePressed(clearCanvas);
+}
+
+function clearCanvas(){
+  clear();
 }
 
 function toHome() {
   window.location = "index.html";
+}
+
+function mouseDragged(){
+  fill('black');
+	ellipse(mouseX, mouseY, 20, 20); 
 }
